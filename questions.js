43c2b2df668848fda9e0129ceb8eb55c31be262e
Isa Hu <usunisa.hu@students.codeinstitute.net>
2024-10-03 12:28:20 +0000

    update css to display questions in column

---
+++ questions.js
@@ -0,0 +1,58 @@
+const questions = [
+    {
+        question: "Which movie won the Academy Award for Best Picture in 1994?",
+        answers: [
+            { text: "Pulp Fiction", correct: false },
+            { text: "The Shawshank Redemption", correct: false },
+            { text: "Forrest Gump", correct: true },
+            { text: "The Lion King", correct: false },
+        ]
+    },
+    {
+        question: "Who played the character of Jack Dawson in the 1997 film Titanic?",
+        answers: [
+            { text: "Leonardo DiCaprio", correct: true },
+            { text: "Brad Pitt", correct: false },
+            { text: "Tom Cruise", correct: false },
+            { text: "Johnny Depp", correct: false },
+        ]
+    },
+    {
+        question: "Which director is known for the movies 'Inception', 'The Dark Knight', and 'Interstellar'?",
+        answers: [
+            { text: "Christopher Nolan", correct: true },
+            { text: "Steven Spielberg", correct: false },
+            { text: "Martin Scorsese", correct: false },
+            { text: "Quentin Tarantino", correct: false },
+        ]
+    },
+    {
+        question: "In which movie does the quote 'May the Force be with you' appear?",
+        answers: [
+            { text: "Star Trek", correct: false },
+            { text: "The Lord of the Rings", correct: false },
+            { text: "Harry Potter", correct: false },
+            { text: "Star Wars", correct: true },
+        ]
+    },
+    {
+        question: "Which actress played the role of Katniss Everdeen in 'The Hunger Games' series?",
+        answers: [
+            { text: "Emma Watson", correct: false },
+            { text: "Scarlett Johansson", correct: false },
+            { text: "Anne Hathaway", correct: false },
+            { text: "Jennifer Lawrence", correct: true },
+        ]
+    },
+    {
+        question: "Which 1994 film featured the song 'Hakuna Matata'?",
+        answers: [
+            { text: "The Lion King", correct: true },
+            { text: "Aladdin", correct: false },
+            { text: "Beauty and the Beast", correct: false },
+            { text: "Pocahontas", correct: false },
+        ]
+    },
+    {
+    }  
+];
