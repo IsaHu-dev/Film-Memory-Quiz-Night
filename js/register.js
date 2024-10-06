94f015bc3a5f276e84b5a341d4eb1bddbcba566e
Isa Hu <usunisa.hu@students.codeinstitute.net>
2024-10-06 15:07:33 +0000

    add line of code /* jshint esversion: 6 */ to pass jshint validator for register.js

--- js/register.js
+++ js/register.js
@@ -1,3 +1,4 @@
+  /* jshint esversion: 6 */
   // Function to restart the quiz
   document.getElementById('restart-btn').addEventListener('click', function() {
     window.location.reload();  // Reload the page to restart the quiz
